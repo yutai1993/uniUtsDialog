a24c91a7717c20b031d7ff6d4199cffa2aa649f2
yutai <yutai1993@yeah.net>
2024-06-16 16:18:39 +0800

    初始化

--- utsDialog/src/main/java/com/qishi/utsdialog/UtsDialog.java
+++ utsDialog/src/main/java/com/qishi/utsdialog/UtsDialog.java
@@ -3,7 +3,7 @@ package com.qishi.utsdialog;
 import com.kongzue.dialogx.dialogs.MessageDialog;
 
 public class UtsDialog {
-    public void dialogShow(){
+    public static void dialogShow(){
         MessageDialog.show("标题", "正文内容", "确定");
     }
 }
