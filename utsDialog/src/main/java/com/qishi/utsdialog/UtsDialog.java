68b93db530a24f8dd716be2ddde9363aaec72df5
yutai <yutai1993@yeah.net>
2024-06-16 16:41:19 +0800

    初始化

--- utsDialog/src/main/java/com/qishi/utsdialog/UtsDialog.java
+++ utsDialog/src/main/java/com/qishi/utsdialog/UtsDialog.java
@@ -1,8 +1,14 @@
 package com.qishi.utsdialog;
 
+import android.content.Context;
+import com.kongzue.dialogx.DialogX;
 import com.kongzue.dialogx.dialogs.MessageDialog;
 
 public class UtsDialog {
+    public static void init(Context context){
+        //初始化
+        DialogX.init(context);
+    }
     public static void dialogShow(){
         MessageDialog.show("标题", "正文内容", "确定");
     }
